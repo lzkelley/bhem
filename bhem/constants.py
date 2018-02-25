468ddafaa5b0d424ac761a2336e37b6db88be50d
lzkelley <lkelley@cfa.harvard.edu>
2018-02-25 18:30:42 -0500

    Add more constants (electron and atomic mass)

--- bhem/constants.py
+++ bhem/constants.py
@@ -21,6 +21,9 @@ SPLC = 2.997924e10            # cm / sec
 # Proton Mass
 MPRT = 1.672621898e-24        # gram
 
+# Electron Mass
+MELC = 9.10938356e-28        # gram
+
 # Planck's constant
 H_PLNK = 6.62607004e-27       # erg sec
 
@@ -30,6 +33,12 @@ K_BLTZ = 1.38064852e-16       # erg / K
 # Year in seconds
 YR = 3.15576e7                # sec
 
+# Atomic Mass Unit (u, Dalton, m_u)
+MAMU = 1.66053904e-24         # g
+
+# Derived quantities
+# ==================
+
 _band_name = ['u', 'b', 'v', 'r', 'i']
 _band_wlen = [365, 445, 551, 658, 806]   # nm
 _band_color = ['violet', 'blue', 'green', 'red', 'darkred']
@@ -37,3 +46,7 @@ Band = namedtuple('band', ['name', 'freq', 'wlen', 'color'])
 
 BANDS = {nn: Band(nn, SPLC/(ll*1e-7), ll*1e-7, cc)
          for nn, ll, cc in zip(_band_name, _band_wlen, _band_color)}
+
+
+# Electron-Scattering (Thomson) Opacity
+KAPPA_ES = SIGMA_T / MPRT     # cm^2 / gram
