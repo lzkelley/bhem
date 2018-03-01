daf09a0fddd5f2a33088246ee44a9f6b06d424dd
lzkelley <lkelley@cfa.harvard.edu>
2018-03-01 12:58:00 -0800

    Load version information in '__init__.py'

--- bhem/__init__.py
+++ bhem/__init__.py
@@ -1,6 +1,19 @@
 """
 """
 
+import os
+import warnings
+
+# Load Version Number from file (as string)
+try:
+    __VERSION_FNAME = "VERSION.txts"
+    CWD = os.path.abspath(os.path.split(__file__)[0])
+    vpath = os.path.join(CWD, os.pardir, __VERSION_FNAME)
+    __version__ = open(vpath, 'r').read().strip()
+except FileNotFoundError as err:
+    warnings.warn("Error loading version file '{}'!  '{}'".format(vpath, str(err)), RuntimeWarning)
+    __version__ = "v?.?.?"
+
 from . import constants
 from . import basics
 
